c2b6a1fe6124d579148cca4e221058b5c2a40d8f
onur-the-king <46904559+onur-the-king@users.noreply.github.com>
2021-02-15 16:40:28 +0300

    asd1

---
+++ simple-web.py
@@ -0,0 +1,14 @@
+import os
+import flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome"
+
+@app.route("/asd")
+def asd():
+    return "asd"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.", port="8080")
